2ee753b7737f638edc5eead263546a44281e3b33
James Lu <jameslu@utexas.edu>
2018-10-24 12:00:56 -0500

    implemented bot1 comment functionality

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 '''
 Created by James Lu.
 Last Modified: 10/22/2018
-Usage: $ python3 app.py <MySQL Password>
+Usage: $ python app.py <MySQL Password>
 '''
 
 import os, sys
@@ -17,17 +17,12 @@ from links import getLinks
 #Main program
 def main():
 
+    # Initialize database and bots
     db_init()
     bot1 = Bot1()
 
-    links = getLinks("I Love It","Kanye West")
-    print(links)
-    #comment("9bl2uu","Yeezy yeezy what's good")
-
-    '''
     while(1):
         bot1.run()
-    '''
 
 #Initialize SQL database to store Reddit posts
 def db_init():
@@ -50,14 +45,12 @@ def db_init():
 class Bot1():
 
     def __init__(self):
-
         redditInit('bot1')
-
 
     def run(self):
 
         #Check for valid response from Reddit API
-        response = json.loads(requests.get("https://www.reddit.com/r/hiphopheads/new.json?sort=new", timeout=5).text)
+        response = json.loads(requests.get("https://www.reddit.com/r/jlu9001/new.json?sort=new", timeout=5).text)
         try:
             if response["data"]:
 
@@ -68,13 +61,6 @@ class Bot1():
                     #Filter new posts for song submissions only and filter out songs exclusive to soundcloud
                     newTitle = child["data"]["title"].lower()
                     if "[fresh]" in newTitle and 'soundcloud' not in child["data"]["url"] and len(newTitle.replace('[fresh]','').split('-')) == 2:
-
-                        print("Title: " + newTitle)
-                        print("Url: " + child["data"]["url"])
-                        artist = newTitle.replace('[fresh]','').split('-')[0].strip()
-                        song = newTitle.replace('[fresh]','').split('-')[1].strip()
-                        links = getLinks(song, artist)
-
 
                         query = 'SELECT post_id FROM posts_replied_to WHERE post_id="{}"'.format(child["data"]["id"])
                         cursor.execute(query)
@@ -87,13 +73,26 @@ class Bot1():
                             song = newTitle.replace('[fresh]','').split('-')[1].strip()
                             id = child["data"]["id"]
 
+                            print("Song: " + song)
+                            print("Artist: " + artist)
+                            print("Getting links")
+
+
                             #Get links from streaming services
                             links = getLinks(song, artist)
 
                             #Comment to post
-                            comment=""
-                            #comment(id, comment)
+                            content="Here are other streaming services that have " + song.upper() + " by " + artist.upper + ":\n***\n" \
+                                    + (("[Spotify](" + links["Spotify"] + ")\n\n") if 'open.spotify' in links["Spotify"] else "Spotify Link Unavailable\n\n") \
+                                    + (("[Apple Music](" + links["Apple Music"] + ")\n\n") if 'itunes.apple' in links["Apple Music"] else "Apple Music Link Unavailable\n\n") \
+                                    + (("[Amazon Music](" + links["Amazon Music"] + ")\n\n") if 'amazon' in links["Amazon Music"] else "Amazon Music Link Unavailable\n\n") \
+                                    + (("[Google Play](" + links["Google Play"] + ")\n\n") if 'play.google' in links["Google Play"] else "Google Play Link Unavailable\n\n") \
+                                    + (("[Tidal](" + links["Tidal"] + ")\n\n") if 'tidal.com' in links["Tidal"] else "Tidal Link Unavailable\n\n") \
+                                    + "***\n^(I am a bot. You can view my source code) ^[here](https://github.com/jlu9001/Reddit-hip-hop-bot)"
 
+                            comment(id, content)
+
+                            print("Serviced id: " + id)
 
                             # Add submission to table of posts that have been replied to
                             query = 'INSERT INTO posts_replied_to (post_id, artist, song) VALUES("{}","{}","{}")'.format(id, artist, song)
